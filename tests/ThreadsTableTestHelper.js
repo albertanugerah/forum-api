aad572f0e020693f654008c3f5de6564620d1992
albertanugerah <albertanugerah@hotmail.com>
2021-09-06 11:44:27 +0700

    create ThreadsTableTestHelper

---
+++ tests/ThreadsTableTestHelper.js
@@ -0,0 +1,52 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const ThreadsTableTestHelper = {
+  async addThread({
+    id = 'thread-123',
+    title = 'sebuah thread',
+    body = 'sebuah body',
+    owner = 'user-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4)',
+      values: [id, title, body, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async findThreadsById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+  async findThreadsByOwner(owner) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE owner = $1',
+      values: [owner],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async verifyThreadsOwner(id, owner) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1 AND owner = $2',
+      values: [id, owner],
+    };
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1=1');
+  },
+};
+
+module.exports = ThreadsTableTestHelper;
