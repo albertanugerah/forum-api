a497db5322c7f33f6d2c6c1af6eb8b4e23480e6e
albertanugerah <albertanugerah@hotmail.com>
2021-09-06 11:19:55 +0700

    complete migrations table threads

--- migrations/1630900447864_create-table-threads.js
+++ migrations/1630900447864_create-table-threads.js
@@ -2,6 +2,30 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
 
-exports.down = pgm => {};
+  // menambahkan foreign key pada owner kolom id dari table users
+  pgm.addConstraint('threads', 'fk_threads.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
