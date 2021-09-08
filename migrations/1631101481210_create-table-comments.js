d1e71d3e2012ca20b216001d1e15dec625c8066a
albertanugerah <albertanugerah@hotmail.com>
2021-09-08 19:06:18 +0700

    create migration table comments

--- migrations/1631101481210_create-table-comments.js
+++ migrations/1631101481210_create-table-comments.js
@@ -2,6 +2,42 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    date: {
+      type: 'DATE',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"users"',
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"threads"',
+    },
+    is_deleted: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: 'false',
+    },
+  });
+  // menambahkan foreign key pada owner kolom id dari table users
+  pgm.addConstraint('comments', 'fk_comments.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('comments', 'fk_comments.thread_id_threads.id', 'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE');
+};
 
-exports.down = pgm => {};
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
