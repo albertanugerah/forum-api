b73167932ea05e603d061dcecd10903b2ddbc064
albertanugerah <albertanugerah@hotmail.com>
2021-09-10 02:07:04 +0700

    add method getOwnerByReplyId and deleteReplyById

--- src/Domains/replies/ReplyRepository.js
+++ src/Domains/replies/ReplyRepository.js
@@ -6,6 +6,14 @@ class ReplyRepository {
   async getReplyByCommentId(commentId) {
     throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async getOwnerByReplyId(id) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteReplyById(id) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ReplyRepository;
