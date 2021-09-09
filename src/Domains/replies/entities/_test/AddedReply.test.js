55fde43fe1143b9da7ce20b11e7d3b99ac843ef8
albertanugerah <albertanugerah@hotmail.com>
2021-09-09 22:45:06 +0700

    create addReply and addedReply entities

---
+++ src/Domains/replies/entities/_test/AddedReply.test.js
@@ -0,0 +1,40 @@
+const AddedReply = require('../AddedReply');
+
+describe('a AddedReply entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      content: 'ini_content',
+    };
+    // Action and Assert
+    expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      id: 123,
+      content: 'ini_content',
+      owner: true,
+    };
+    // Action and Assert
+    expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create AddedReply object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'thread-123',
+      content: 'ini_content',
+      owner: 'user-123',
+    };
+
+    // Action
+    const createdThread = new AddedReply(payload);
+
+    // Assert
+    expect(createdThread.id).toEqual(payload.id);
+    expect(createdThread.content).toEqual(payload.content);
+    expect(createdThread.owner).toEqual(payload.owner);
+  });
+});
